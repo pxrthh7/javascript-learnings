3e729af509a94069af70636c63153a26940e48bc
patilparthh29 <patilparthh29@gmail.com>
2025-12-03 22:29:23 +0530

    Variable commit

---
+++ variables/variables.js
@@ -0,0 +1,4 @@
+const aadharNumber = "12345678923" // For constants, define while declaration, cant change value
+let name = "Parth"  // can change the value, have block scope
+var age = 22    // avoid this, no block scope, 
+email = "parth@email.com"   // avoid, treated as global variable my js
